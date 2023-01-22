05876f66e8a02340a474d615b938c9766b74ab0b
Thomas Palmer <thomas.palmer.developer@gmail.com>
2023-01-22 21:25:24 +0000

    Remove files

--- stubs/routes/frontend-routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/verify-account/{id}/{hash}')
-    ->name('frontend.verification.verify');
-
-Route::get('/password/reset/{token}')
-    ->name('password.reset');
--- stubs/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::view('/{path?}', 'app')
-    ->where('path', '^(?:(?!\/api\/).)*$')
-    ->name('react');
-
-include dirname(__FILE__).'/frontend-routes.php';
